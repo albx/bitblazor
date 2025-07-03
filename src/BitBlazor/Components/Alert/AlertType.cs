87a39625e833c0587cb67d182aabe10cdf175734
Alberto Mori <albi.mo87@hotmail.it>
2025-07-03 22:20:26 +0200

    #2 - add alert component

---
+++ src/BitBlazor/Components/Alert/AlertType.cs
@@ -0,0 +1,32 @@
+﻿namespace BitBlazor.Components;
+
+/// <summary>
+/// Specifies the type of an alert component.
+/// </summary>
+public enum AlertType
+{
+    /// <summary>
+    /// Primary alert
+    /// </summary>
+    Primary,
+
+    /// <summary>
+    /// Info alert
+    /// </summary>
+    Info,
+
+    /// <summary>
+    /// Success alert
+    /// </summary>
+    Success,
+
+    /// <summary>
+    /// Warning alert
+    /// </summary>
+    Warning,
+
+    /// <summary>
+    /// Danger alert
+    /// </summary>
+    Danger
+}
---
+++ src/BitBlazor/Components/Alert/BitAlert.razor.cs
@@ -0,0 +1,48 @@
+using Microsoft.AspNetCore.Components;
+
+namespace BitBlazor.Components;
+
+/// <summary>
+/// Represents an alert component using Bootstrap Italia styles.
+/// </summary>
+public partial class BitAlert
+{
+    /// <summary>
+    /// Gets or sets the type of the alert
+    /// </summary>
+    [Parameter]
+    [EditorRequired]
+    public AlertType Type { get; set; }
+
+    /// <summary>
+    /// Gets or sets the child content of the component
+    /// </summary>
+    [Parameter]
+    [EditorRequired]
+    public RenderFragment ChildContent { get; set; } = default!;
+
+    private string ComputedCssClasses => $"alert {ComputeCssClasses()}".Trim();
+
+    private string ComputeCssClasses()
+    {
+        var cssClasses = new List<string>();
+        AddTypeClass(cssClasses);
+
+        return string.Join(" ", cssClasses);
+    }
+
+    private void AddTypeClass(List<string> cssClasses)
+    {
+        var typeClass = Type switch
+        {
+            AlertType.Primary => "alert-primary",
+            AlertType.Info => "alert-info",
+            AlertType.Success => "alert-success",
+            AlertType.Warning => "alert-warning",
+            AlertType.Danger => "alert-danger",
+            _ => string.Empty
+        };
+
+        cssClasses.Add(typeClass);
+    }
+}
