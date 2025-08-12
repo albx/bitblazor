17750e60c47a093bb208b9aea5dec4c47425a0dd
albx <albi.mo87@hotmail.it>
2025-08-12 18:49:12 +0200

    #4 - add more stories for card

--- src/BitBlazor/Components/Card/CardDate.razor.cs
+++ src/BitBlazor/Components/Card/CardDate.razor.cs
@@ -34,7 +34,35 @@ public partial class CardDate
     [EditorRequired]
     public string DisplayFormat { get; set; } = string.Empty;
 
+    /// <summary>
+    /// Gets or sets the color of the text
+    /// </summary>
+    [Parameter]
+    public Color? TextColor { get; set; }
+
     private string FormattedDatetimeValue => string.IsNullOrEmpty(DatetimeFormat) ? Date.ToString("yyyy-MM-dd") : Date.ToString(DatetimeFormat);
 
     private string FormattedDisplayDate => string.IsNullOrEmpty(DisplayFormat) ? Date.ToString() : Date.ToString(DisplayFormat);
+
+    private string ComputeCssClasses()
+    {
+        List<string> cssClasses = ["it-card-date"];
+
+        if (TextColor.HasValue)
+        {
+            var textColorClass = TextColor.Value switch
+            {
+                Color.Primary => "text-primary",
+                Color.Secondary => "text-secondary",
+                Color.Warning => "text-warning",
+                Color.Success => "text-success",
+                Color.Danger => "text-danger",
+                _ => string.Empty
+            };
+
+            cssClasses.Add(textColorClass);
+        }
+
+        return string.Join(" ", cssClasses);
+    }
 }
--- src/BitBlazor/Components/Card/CardFooter.razor.cs
+++ src/BitBlazor/Components/Card/CardFooter.razor.cs
@@ -15,4 +15,42 @@ public partial class CardFooter
     /// </summary>
     [Parameter]
     public RenderFragment ChildContent { get; set; } = default!;
+
+    /// <summary>
+    /// Gets or sets the it-card-related class
+    /// </summary>
+    [Parameter]
+    public bool CardRelated { get; set; } = true;
+
+    /// <summary>
+    /// Gets or sets the aria-label attribute
+    /// </summary>
+    [Parameter]
+    public string? AriaLabel { get; set; }
+
+    private Dictionary<string, object> attributes = new();
+
+    protected override void OnParametersSet()
+    {
+        if (!string.IsNullOrWhiteSpace(AriaLabel))
+        {
+            attributes["aria-label"] = AriaLabel;
+        }
+        else
+        {
+            attributes.Remove("aria-label");
+        }
+    }
+
+    private string ComputeCssClasses()
+    {
+        List<string> cssClasses = ["it-card-footer"];
+
+        if (CardRelated)
+        {
+            cssClasses.Add("it-card-related");
+        }
+
+        return string.Join(" ", cssClasses);
+    }
 }
