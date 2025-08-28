f1f2cfb9f461e519d0f46b2c4a4b3681d7062bfb
Alberto Mori <albi.mo87@hotmail.it>
2025-08-28 07:23:02 +0200

    #21 - add input placeholder

--- src/BitBlazor/Core/BitFormComponentBase.cs
+++ src/BitBlazor/Core/BitFormComponentBase.cs
@@ -63,6 +63,12 @@ public abstract class BitFormComponentBase<T> : BitComponentBase
     public bool Disabled { get; set; }
 
     /// <summary>
+    /// Gets or sets the placeholder to show in the component
+    /// </summary>
+    [Parameter]
+    public string? Placeholder { get; set; }
+
+    /// <summary>
     /// Gets the prefix used to generate the unique Id of the component
     /// </summary>
     protected abstract string FieldIdPrefix { get; }
@@ -74,8 +80,6 @@ public abstract class BitFormComponentBase<T> : BitComponentBase
     /// This property will validate the <typeparamref name="T"/> type in the costructor.
     /// </remarks>
     protected abstract Type[] SupportedTypes { get; }
-
-    private bool isLabelActive = false;
 
     /// <summary>
     /// Construct the <see cref="BitFormComponentBase{T}"/> instance
@@ -96,20 +100,20 @@ public abstract class BitFormComponentBase<T> : BitComponentBase
     {
         base.OnParametersSet();
         SetRequiredAttribute();
-        SetInitialLabelState();
+        SetPlaceholderAttribute();
     }
 
-    private void SetInitialLabelState()
+    private void SetPlaceholderAttribute()
     {
-        isLabelActive = !ValueIsEmpty();
+        if (!string.IsNullOrWhiteSpace(Placeholder))
+        {
+            AdditionalAttributes["placeholder"] = Placeholder;
+        }
+        else
+        {
+            AdditionalAttributes.Remove("placeholder");
+        }
     }
-
-    /// <summary>
-    /// Determines whether the current value is considered empty.
-    /// </summary>
-    /// <remarks>The definition of "empty" is determined by the implementing class.</remarks>
-    /// <returns><see langword="true"/> if the value is empty; otherwise, <see langword="false"/>.</returns>
-    protected abstract bool ValueIsEmpty();
 
     private void SetRequiredAttribute()
     {
@@ -134,32 +138,5 @@ public abstract class BitFormComponentBase<T> : BitComponentBase
         }
 
         AdditionalAttributes["id"] = Id!;
-    }
-
-    /// <summary>
-    /// Sets the label's active state.
-    /// </summary>
-    /// <param name="active">
-    /// A value indicating whether the label should be marked as active.
-    /// <see langword="true"/> to mark the label as active; otherwise, <see langword="false"/>.
-    /// </param>
-    protected void SetLabelAsActive(bool active) => isLabelActive = active;
-
-    /// <summary>
-    /// Computes the CSS class string for a label based on its current state.
-    /// </summary>
-    /// <returns>
-    /// A string containing the CSS class for the label. 
-    /// Returns "active" if the label is in an active state; otherwise, returns an empty string.
-    /// </returns>
-    protected string ComputeLabelCssClass()
-    {
-        var builder = new CssClassBuilder();
-        if (isLabelActive)
-        {
-            builder.Add("active");
-        }
-
-        return builder.Build();
     }
 }
--- src/BitBlazor/Form/TextField/BitTextField.razor.cs
+++ src/BitBlazor/Form/TextField/BitTextField.razor.cs
@@ -17,6 +17,30 @@ public partial class BitTextField : BitFormComponentBase<string?>
     /// <inheritdoc/>
     protected override Type[] SupportedTypes { get; } = [typeof(string)];
 
+    private bool isLabelActive = false;
+
     /// <inheritdoc/>
-    protected override bool ValueIsEmpty() => string.IsNullOrEmpty(Value);
+    protected override void OnParametersSet()
+    {
+        base.OnParametersSet();
+        SetInitialLabelState();
+    }
+
+    private void SetInitialLabelState()
+    {
+        isLabelActive = !string.IsNullOrEmpty(Value) || !string.IsNullOrWhiteSpace(Placeholder);
+    }
+
+    private void SetLabelAsActive(bool active) => isLabelActive = active;
+
+    private string ComputeLabelCssClass()
+    {
+        var builder = new CssClassBuilder();
+        if (isLabelActive)
+        {
+            builder.Add("active");
+        }
+
+        return builder.Build();
+    }
 }
