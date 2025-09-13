d5dae522f9cea8b9e8017a6307adcd1ce86decb9
albx <albi.mo87@hotmail.it>
2025-09-13 11:27:13 +0200

    #21 - fix test and add some test for pasword field

--- tests/BitBlazor.Test/Form/PasswordField/BitPasswordFieldTest.Behaviors.cs
+++ tests/BitBlazor.Test/Form/PasswordField/BitPasswordFieldTest.Behaviors.cs
@@ -56,4 +56,90 @@ public class BitPasswordFieldTest
         Assert.Contains("d-none", passwordInvisibleIcon.ClassList);
         Assert.DoesNotContain("d-none", passwordVisibleIcon.ClassList);
     }
+
+    [Fact]
+    public void BitPasswordField_Should_Set_Active_Class_To_Label_On_Focus()
+    {
+        using var ctx = new TestContext();
+
+        string? value = null;
+
+        var component = ctx.RenderComponent<BitPasswordField>(parameters => parameters
+            .Add(p => p.Label, "label")
+            .Bind(p => p.Value, value, v => value = v));
+
+        var label = component.Find("label");
+        Assert.DoesNotContain("active", label.ClassList);
+
+        var input = component.Find("input.form-control");
+        input.Focus();
+
+        Assert.Contains("active", label.ClassList);
+    }
+
+    [Fact]
+    public void BitPasswordField_Should_Set_Active_Class_To_Label_On_Blur_If_Value_Is_Not_Empty()
+    {
+        using var ctx = new TestContext();
+
+        string? value = null;
+
+        var component = ctx.RenderComponent<BitPasswordField>(parameters => parameters
+            .Add(p => p.Label, "label")
+            .Bind(p => p.Value, value, v => value = v));
+
+        var label = component.Find("label");
+        Assert.DoesNotContain("active", label.ClassList);
+
+        component.SetParametersAndRender(
+            parameters => parameters.Add(p => p.Value, "new value"));
+
+        var input = component.Find("input.form-control");
+        input.Blur();
+
+        Assert.Contains("active", label.ClassList);
+    }
+
+    [Fact]
+    public void BitPasswordField_Should_Set_Active_Class_To_Label_On_Change_If_Value_Is_Not_Empty()
+    {
+        using var ctx = new TestContext();
+
+        string? value = null;
+
+        var component = ctx.RenderComponent<BitPasswordField>(parameters => parameters
+            .Add(p => p.Label, "label")
+            .Bind(p => p.Value, value, v => value = v));
+
+        var label = component.Find("label");
+        Assert.DoesNotContain("active", label.ClassList);
+
+        component.SetParametersAndRender(
+            parameters => parameters.Add(p => p.Value, "new value"));
+
+        Assert.Contains("active", label.ClassList);
+    }
+
+    [Fact]
+    public void BitPasswordField_Should_Remove_Active_Class_From_Label_On_Blur_If_Value_Is_Empty()
+    {
+        using var ctx = new TestContext();
+
+        string? value = "initial value";
+
+        var component = ctx.RenderComponent<BitPasswordField>(parameters => parameters
+            .Add(p => p.Label, "label")
+            .Bind(p => p.Value, value, v => value = v));
+
+        var label = component.Find("label");
+        Assert.Contains("active", label.ClassList);
+
+        component.SetParametersAndRender(
+            parameters => parameters.Add(p => p.Value, null));
+
+        var input = component.Find("input.form-control");
+        input.Blur();
+
+        Assert.DoesNotContain("active", label.ClassList);
+    }
 }
