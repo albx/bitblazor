869a486dcf38e62e684a001381ca7f1bef7165e1
albx <albi.mo87@hotmail.it>
2025-10-24 17:52:08 +0200

    #25 - add getting started section to readme, add missing avatar component summary in docs, update version to the new one

--- stories/BitBlazor.Stories/Program.cs
+++ stories/BitBlazor.Stories/Program.cs
@@ -25,7 +25,7 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.MapBlazingStoryMcp();
+app.MapBlazingStoryMcp(pattern: "mcp");
 app.MapStaticAssets();
 app.UseRouting();
 app.UseAntiforgery();
