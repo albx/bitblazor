87f25307719b63273546bdaa2666f0850906fed6
albx <albi.mo87@hotmail.it>
2025-11-04 06:39:00 +0100

    #23 - add comments, add some tests to validate value binding

--- src/BitBlazor/Form/Datepicker/BitDatepicker.razor.cs
+++ src/BitBlazor/Form/Datepicker/BitDatepicker.razor.cs
@@ -1,5 +1,15 @@
 namespace BitBlazor.Form;
 
+/// <summary>
+/// Represents a date picker input component that allows users to select a date value of a specified type.
+/// </summary>
+/// <remarks>
+/// Use this component to provide a user interface for date selection in forms or other data entry scenarios. 
+/// The generic type parameter restricts the value to supported date types, ensuring type safety.
+/// </remarks>
+/// <typeparam name="T">
+/// The type of the value to be selected. Supported types are <see cref="DateTime"/> and <see cref="DateOnly"/>.
+/// </typeparam>
 public partial class BitDatepicker<T> : BitInputFieldBase<T>
 {
     /// <inheritdoc/>
