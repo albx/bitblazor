b276765e2b305167c1c47794ddf2c8d6b84ecde4
Alessio Marinelli <defkon1@gmail.com>
2026-01-29 10:43:47 +0100

    [Feature] Add breadcrumb component - Close #7 (#69)
    * Added CodeQL
    
    Params:
    - ignore push on main
    - triggered by pull request on main
    - triggered manually
    
    * Update CodeQL to v3
    
    Updated deprecated action from v2 to v3
    
    Ref: https://github.blog/changelog/2025-01-10-code-scanning-codeql-action-v2-is-now-deprecated/
    
    * Create Test Runner action
    
    Params:
    - triggered pull request on main
    - triggered manually
    
    * Fixed Test Runner
    
    Set the correct SDK version
    Removed NerdBank Git Versioning step
    
    * Added Test Runner and CodeQL to README.md
    
    * Added base component
    
    * Enhanced BitBreadcrumb component
    Added rendering tests
    Added docs
    
    * Added review suggestions (parameters nullability and default values)

---
+++ src/BitBlazor/Components/Breadcrumb/BitBreadcrumb.razor.cs
@@ -0,0 +1,53 @@
+﻿using BitBlazor.Core;
+using Microsoft.AspNetCore.Components;
+
+namespace BitBlazor.Components;
+
+/// <summary> 
+/// BitBreadcrumb renders a breadcrumb navigation component to display the current location within a hierarchy.
+/// The component supports customization of items, separators, and accessibility features.
+/// </summary>
+public partial class BitBreadcrumb : BitComponentBase
+{
+    /// <summary>
+    /// Gets or sets the ARIA label for the breadcrumb component.
+    /// </summary>
+    [Parameter]
+    public string? Label { get; set; }
+
+    /// <summary>
+    /// Gets or sets the items shown on the breadcrumb.
+    /// </summary>
+    [Parameter]
+    public IReadOnlyList<BitBreadcrumbItem> Items { get; set; } = Enumerable.Empty<BitBreadcrumbItem>().ToList();
+
+    /// <summary>
+    /// Gets or sets the separator to show between breadcrumb components.
+    /// </summary>
+    [Parameter]
+    public string? Separator { get; set; } = "/";
+
+    /// <summary>
+    /// Gets or sets a value indicating whether the component is rendered on a dark background, or not.
+    /// </summary>
+    [Parameter]
+    public bool Dark { get; set; } = false;
+
+    private string ComputeCssClasses()
+    {
+        var builder = new CssClassBuilder("breadcrumb");
+
+        if (Dark)
+        {
+            builder.AddRange(new[] { "dark", "px-3" });
+        }
+
+        return builder.Build();
+    }
+
+    private bool IsLeaf(BitBreadcrumbItem? item)
+    {
+        return item is not null && Items?.LastOrDefault() == item;
+    }
+}
+
---
+++ src/BitBlazor/Components/Breadcrumb/BitBreadcrumbItem.cs
@@ -0,0 +1,28 @@
+﻿namespace BitBlazor.Components;
+
+/// <summary>
+/// Represents an item in a <see cref="BitBreadcrumb"/> navigation component.
+/// </summary>
+public class BitBreadcrumbItem
+{
+    /// <summary>
+    /// Gets or sets the text to display for the item.
+    /// </summary>
+    public string Text { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Gets or sets the icon name to display before the item.
+    /// </summary>
+    public string? Icon { get; set; }
+
+    /// <summary>
+    /// Gets or sets the URL to link to the item.
+    /// </summary>
+    public string? Link { get; set; }
+
+    /// <summary>
+    /// Gets a value indicating whether the item has an icon.
+    /// </summary>
+    /// <returns><code>true</code> if the item has an icon specified, <code>false</code> otherwise</returns>
+    public bool HasIcon() => !string.IsNullOrEmpty(Icon);
+}
