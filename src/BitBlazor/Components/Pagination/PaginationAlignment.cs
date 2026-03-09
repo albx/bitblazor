551b6c0faaa0ec631607fba0a722d1a84c7b3490
Alberto Mori <albi.mo87@hotmail.it>
2026-03-09 22:28:43 +0100

    #8 - pagination alignment

---
+++ src/BitBlazor/Components/Pagination/PaginationAlignment.cs
@@ -0,0 +1,26 @@
+﻿namespace BitBlazor.Components;
+
+/// <summary>
+/// Specifies the alignment of pagination elements within a layout.
+/// </summary>
+/// <remarks>
+/// This enumeration allows for the alignment of pagination controls to be specified as either left, center, or right. 
+/// It is useful for customizing the appearance of pagination in user interfaces.
+/// </remarks>
+public enum PaginationAlignment
+{
+    /// <summary>
+    /// Left alignment (Default)
+    /// </summary>
+    Left,
+
+    /// <summary>
+    /// Center alignment
+    /// </summary>
+    Center,
+
+    /// <summary>
+    /// Right alignment
+    /// </summary>
+    Right
+}
